21bd2d635060fbaae3f4748ea104035dc68be4f4
Jacob <your_email@real_email.edu>
2022-03-16 12:42:12 -0500

    username displayed when loggeed in

--- src/components/LoginSignUpLogic.jsx
+++ src/components/LoginSignUpLogic.jsx
@@ -2,41 +2,33 @@ import React, { useEffect } from "react";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import { userData } from "../api";
-import { Redirect } from "react-router-dom";
 import { useState } from "react";
 
-const BASE_URL =
-  "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT";
-
 const LoginSignUp = ({ isLoggedIn, setToken, setIsLoggedIn }) => {
-  // const user = userData();
-  // console.log(user);
-  const [Tok, setTo] = useState(0);
-  const setTok = () => {
-    setToken(" ");
+
+  const clearToken = () => {
+    setToken("");
     localStorage.removeItem("token");
-    setTo(1);
     setIsLoggedIn(false);
-    // return <Redirect push to={{ pathname: "/Login" }} />;
   };
+
+  
+  const [username, setUsername] = useState('')
 
   useEffect(() => {
-    console.log("TEST");
-  }, [Tok]);
-  // console.log(username, "username")
-  const getUserName = async () => {
-    const result = await userData(localStorage.getItem("token"));
-    const shownUser = result.data.username;
-    console.log(shownUser);
+    const getUserName = async () => {
+      const result = await userData(localStorage.getItem("token"));
+      setUsername (result.data.username)
+    };
+    getUserName()
+  }, [])
 
-    return shownUser;
-  };
   return (
     <>
       {isLoggedIn ? (
         <div>
-          <div>{`Welcome Back ${() => {}}`}</div>
-          <button className="logOut" onClick={() => setTok()}>
+          <div>{`Welcome Back ${username}`}</div>
+          <button className="logOut" onClick={() => clearToken()}>
             Log Out
           </button>
         </div>
